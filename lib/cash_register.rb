b874b0f884db41eae926a839a4ac0b8ac777e2e8
Philip Warren <philwarren1@gmail.com>
2020-08-11 14:36:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
-
+class CashRegister 
+  attr_accessor :total, :title, :price, :discount
+  
+  
+  
+  def initialize
+    @total = 0
+    @discount = @total - 20%
+ end
+ 
+ def total
+   @total
+ end
+ 
+ def add_item
+   @title
+   @price
+   @total
+ end
+ 
+ def apply_discount
+ end
+ 
+ def items
+ end
+ 
+ def void_last_transaction
+ end
+ 
+end
